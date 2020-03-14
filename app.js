d9370475bd96422d0ed364c1abdce72494b945c6
James Berke <james.berke@gmail.com>
2020-03-14 16:16:55 -0700

    established routes and created user auth

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 const app = express();
+const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
-const db = require("./config/keys").mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+.connect(db, { useNewUrlParser: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
